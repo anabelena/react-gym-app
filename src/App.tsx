dad1883f9bf8b64c13b3e3eef859f16bc09b9bbe
anabelena <anabelen.aristah@gmail.com>
2025-03-28 21:15:10 -0700

    feat: - added enum SelectedPage - added NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import NavBar from "@/scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "./shared/types";
+
 
 function App() {
-
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
   return (
-    <div className="app bg-gray-50">
-         <NavBar/>
+    <div className="app bg-gray-20">
+      <NavBar 
+      selectedPage={selectedPage} 
+      setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
