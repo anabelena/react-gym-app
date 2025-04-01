7890527592febcfc6c8e3d1a86a77462e63a88e9
anabelena <anabelen.aristah@gmail.com>
2025-03-31 19:24:06 -0700

    feat: ActionButton created to manage Become a Member action on the Nav

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -12,7 +13,8 @@ type Props = {
 
 const NavBar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
-  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)")
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
   return (
     <nav className="">
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -21,34 +23,53 @@ const NavBar = ({ selectedPage, setSelectedPage }: Props) => {
             {/* LEFT SIDE */}
             <img alt="logo" src={Logo} />
             {/* RIGHT SIDE */}
-            <div className={`${flexBetween} w-full `}>
-              <div className={`${flexBetween} gap-8 text-sm`}>
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Benefits"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Our Classes"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Contact Us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+            {isAboveMediumScreens ? (
+              //Desktop
+              <div className={`${flexBetween} w-full `}>
+                {/* Menu Options */}
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                {/* Menu Buttons */}
+                <div className={`${flexBetween} gap-8`}>
+                  <p> Sign in </p>
+                  <ActionButton 
+                  setSelectedPage={setSelectedPage}> 
+                  Become a Member 
+                  </ActionButton>
+                </div>
               </div>
-              <div className={`${flexBetween} gap-8`}>
-                <p> Sign in </p>
-                <button>Become a Member</button>
+            ) : (
+              //Mobile Screen
+              <div>
+                <button
+                  aria-label="Open menu"
+                  className="rounded-full bg-secondary-500 p-2"
+                  onClick={() => setIsMenuToggled(!isMenuToggled)}
+                >
+                  <Bars3Icon className="h-6 w-6 text-white " />
+                </button>
               </div>
-            </div>
+            )}
           </div>
           <div></div>
         </div>
