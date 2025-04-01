77a77fd9738d652bb69db6bda54e7fcde65c44f4
anabelena <anabelen.aristah@gmail.com>
2025-03-31 20:18:27 -0700

    feat: added Mobile Menu Modal

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
@@ -18,7 +18,7 @@ const NavBar = ({ selectedPage, setSelectedPage }: Props) => {
   return (
     <nav className="">
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
-        <div className={`${flexBetween} mx-auto w-5/6`}>
+        <div className={`${flexBetween} mx-auto w-5/6 `}>
           <div className={`${flexBetween} w-full gap-16`}>
             {/* LEFT SIDE */}
             <img alt="logo" src={Logo} />
@@ -52,14 +52,13 @@ const NavBar = ({ selectedPage, setSelectedPage }: Props) => {
                 {/* Menu Buttons */}
                 <div className={`${flexBetween} gap-8`}>
                   <p> Sign in </p>
-                  <ActionButton 
-                  setSelectedPage={setSelectedPage}> 
-                  Become a Member 
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
                   </ActionButton>
                 </div>
               </div>
             ) : (
-              //Mobile Screen
+              //Mobile
               <div>
                 <button
                   aria-label="Open menu"
@@ -71,9 +70,45 @@ const NavBar = ({ selectedPage, setSelectedPage }: Props) => {
               </div>
             )}
           </div>
-          <div></div>
         </div>
       </div>
+      {/* MOBILE MENU MODAL */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 h-full z-40 w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* CLOSE ICON */}
+          <div className="flex justify-end p-12 ">
+            <button
+              aria-label="Close menu"
+              onClick={() => setIsMenuToggled(!isMenuToggled)}
+            >
+              <XMarkIcon className="w-6 h-6 text-gray-400" />
+            </button>
+          </div>
+          {/* MENU ITEMS */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
