f70af0a8c85bf659a7eb76321d8d805eacb2a000
anabelena <anabelen.aristah@gmail.com>
2025-04-02 16:12:52 -0700

    style:added responsiveness to Home Component

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -57,9 +57,9 @@ const Home = ({ setSelectedPage }: Props) => {
       </div>
       {/* SPONSORS */}
       {isAboveMediumScreens && (
-        <div>
-          <div>
-            <div>
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
               <img src={SponsorRedBull} alt="redbull-sponsor" />
               <img src={SponsorForbes} alt="redbull-forbes" />
               <img src={SponsorFortune} alt="redbull-fortune" />
