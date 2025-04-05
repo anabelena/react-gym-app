24a0c936b29749f701326d3cfbf23fbde14df0c5
anabelena <anabelen.aristah@gmail.com>
2025-04-04 20:16:50 -0700

    feat(benefit) added animation

--- src/scenes/benefits/benefit.tsx
+++ src/scenes/benefits/benefit.tsx
@@ -1,5 +1,11 @@
 import { SelectedPage } from "@/shared/types";
 import { JSX } from "react";
+import { motion } from "framer-motion";
+
+const childVariant = {
+  hidden:{opacity:0,scale:0.9},
+  visible:{opacity:1,scale:1}
+}
 
 type Props = {
   icon: JSX.Element;
@@ -10,15 +16,20 @@ type Props = {
 
 const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
   return (
-    <div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center">
+    <motion.div 
+    className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center"
+    variants={childVariant}>
+      {/* ICON */}
       <div className="mb-4 flex justify-center ">
         <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
           {icon}
         </div>
       </div>
-
+      {/* TITLE */}
       <h4 className="font-bold "> {title} </h4>
+      {/* DESCRIPTION */}
       <p className="my-3"> {description}</p>
+      {/* LINK */}
       <a
         className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
         onClick={() => setSelectedPage(SelectedPage.ContactUs)}
@@ -26,7 +37,7 @@ const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
       >
         <p>Learn more</p>
       </a>
-    </div>
+    </motion.div>
   );
 };
 
--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -7,29 +7,40 @@ import {
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 import Benefit from "./benefit";
+import ActionButton from "@/shared/ActionButton";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6 " />,
+    title: "State of the Art Facilities",
+    description:
+      "From high-tech machines to spacious workout areas, we ensure a clean, modern, and motivating environment where you can train with confidence. Whether you’re lifting, running, stretching, or recovering, our state-of-the-art setup is built to help you achieve peak performance.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6 " />,
+    title: "100's of Diverse Classes",
+    description:
+      "From high-tech machines to spacious workout areas, we ensure a clean, modern, and motivating environment where you can train with confidence. Whether you’re lifting, running, stretching, or recovering, our state-of-the-art setup is built to help you achieve peak performance.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6 " />,
+    title: "Experts and Pro Trainers",
+    description:
+      "From high-tech machines to spacious workout areas, we ensure a clean, modern, and motivating environment where you can train with confidence. Whether you’re lifting, running, stretching, or recovering, our state-of-the-art setup is built to help you achieve peak performance.",
+  },
+];
 
-const benefits:Array<BenefitType> = [
-    {
-        icon:<HomeModernIcon className="h-6 w-6 "/>,
-        title:"State of the Art Facilities",
-        description:"From high-tech machines to spacious workout areas, we ensure a clean, modern, and motivating environment where you can train with confidence. Whether you’re lifting, running, stretching, or recovering, our state-of-the-art setup is built to help you achieve peak performance."
-    },
-    {
-        icon:<UserGroupIcon className="h-6 w-6 "/>,
-        title:"100's of Diverse Classes",
-        description:"From high-tech machines to spacious workout areas, we ensure a clean, modern, and motivating environment where you can train with confidence. Whether you’re lifting, running, stretching, or recovering, our state-of-the-art setup is built to help you achieve peak performance."
-    },
-    {
-        icon:<AcademicCapIcon className="h-6 w-6 "/>,
-        title:"Experts and Pro Trainers",
-        description:"From high-tech machines to spacious workout areas, we ensure a clean, modern, and motivating environment where you can train with confidence. Whether you’re lifting, running, stretching, or recovering, our state-of-the-art setup is built to help you achieve peak performance."
-    }
-] 
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
 
 const Benefits = ({ setSelectedPage }: Props) => {
   return (
@@ -39,7 +50,17 @@ const Benefits = ({ setSelectedPage }: Props) => {
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
         {/* HEADER */}
-        <div className="md:my-5 md:w-3/5">
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           <HText>MORE THAN JUST A GYM.</HText>
           <p className="my-5 text-sm text-balance">
             At <span className="font-bold">EVOGYM</span> we believe fitness is
@@ -48,18 +69,97 @@ const Benefits = ({ setSelectedPage }: Props) => {
             create a space where everyone, from beginners to athletes, feels
             empowered to push limits, break barriers, and transform their lives.
           </p>
-        </div>
+        </motion.div>
         {/* BENEFITS */}
-        <div className="md:flex items-center justify-between gap-8 mt-5">
-          {benefits.map(( benefit ) => (
+        <motion.div
+          className="md:flex items-center justify-between gap-8 mt-5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {benefits.map((benefit) => (
             <Benefit
-            key={benefit.title}
-            icon={benefit.icon}
-            title={benefit.title}
-            description={benefit.description}
-            setSelectedPage={setSelectedPage}
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
             />
           ))}
+        </motion.div>
+
+        {/* GRAPHICS & DESCRIPTION */}
+        <div className="mt-24 md:flex items-center justify-between ">
+          {/* GRAPHIC */}
+          <img
+            className="mx-auto"
+            src={BenefitsPageGraphic}
+            alt="benefits-page-graphic"
+          />
+          {/* DESCRIPTION */}
+          <div className="md:basis-3/5">
+            {/* TITLE */}
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
+                <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, x: 50 },
+                  visible: { opacity: 1, x: 0 },
+                }}>
+                  <HText>
+                    {" "}
+                    MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500"> FIT </span>{" "}
+                  </HText>
+                </motion.div>
+              </div>
+            </div>
+
+            {/* DESCRIPTION */}
+            <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{delay:0.3, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}>
+              <p className="my-5 text-pretty">
+                Join a global movement of energized individuals who are
+                transforming their lives, one workout at a time. With millions
+                of members around the world, our fitness community thrives on
+                positivity, progress, and real results. Whether you’re hitting
+                the gym, tuning in from home, or taking your first steps toward
+                a healthier lifestyle, you’re never doing it alone. From
+                personalized plans to expert coaching, everything is designed to
+                meet you where you are — and help you get where you want to be.
+              </p>
+              <p className="mb-5 text-pretty">
+                What sets us apart? It’s the energy. The motivation. The
+                celebration of every milestone, big or small. Our members don’t
+                just show up — they show out, support each other, and keep
+                pushing forward together. With flexible programs, engaging
+                challenges, and a culture built on encouragement, it’s easy to
+                see why millions are smiling, sweating, and sticking with it.
+                This isn’t just fitness — it’s a feel-good revolution.
+              </p>
+            </motion.div>
+
+            {/* BUTTON */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-30 before:right-20  before:z[-1] before:content-sparkles ">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join now !
+                </ActionButton>
+              </div>
+            </div>
+          </div>
         </div>
       </motion.div>
     </section>
