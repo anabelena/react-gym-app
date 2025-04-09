90e0c7a436b70002da5fb74105e16432e3a7b44c
anabelena <anabelen.aristah@gmail.com>
2025-04-08 19:28:24 -0700

    feat:added ourclasses sidebar

--- src/scenes/ourclasses/Class.tsx
+++ src/scenes/ourclasses/Class.tsx
@@ -5,13 +5,15 @@ type Props = {
 };
 
 function Class({ name, description, image }: Props) {
+  const overlayStyles = `p-5 absolute z-30 flex 
+    h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 tansition duration-500 hover:opacity-90`;
   return (
     <li className="relative mx-5 inline-block h-[380px] w-[450px]">
-      <div>
+      <div className={overlayStyles}>
         <p>{name}</p>
         <p>{description}</p>
-        <img src={image} alt={`${image}`} />
       </div>
+      <img src={image} alt={`${image}`} />
     </li>
   );
 }
