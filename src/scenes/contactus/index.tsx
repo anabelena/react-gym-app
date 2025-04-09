ee1f439ae131c9b9483b00f6ef155a8da88bfabc
anabelena <anabelen.aristah@gmail.com>
2025-04-09 13:03:18 -0700

    feat:Added ContactUs component

---
+++ src/scenes/contactus/index.tsx
@@ -0,0 +1,13 @@
+import { SelectedPage } from "@/shared/types"
+
+type Props = {
+    setSelectedPage:(value:SelectedPage)=>void,
+}
+
+function ContactUs({setSelectedPage}:Props) {
+  return (
+    <div>Contact us </div>
+  )
+}
+
+export default ContactUs
